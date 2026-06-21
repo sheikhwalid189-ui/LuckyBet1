a89e24f241961d58c386c60ec727b4f6e8d8144c
sheikhwalid189-ui <sheikhwalid189@gmail.com>
2026-06-20 22:08:11 -0700

    Add files via upload

---
+++ Admin.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Admin() {
+  return (
+    <div>
+      <h2>Admin Dashboard</h2>
+
+      <ul>
+        <li>Total Users: 120</li>
+        <li>Total Clicks: 540</li>
+        <li>Affiliate Earnings: $320</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Admin;
